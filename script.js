054ec2dfcda344412e030ff5d01386f2d7009e68
Vidotti <vidottizzz@gmail.com>
2023-01-17 17:22:04 -0300

    1st card disappear when click on submit, and just the 1st number is working

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
-function mostra() {
-    document.getElementById('card-content').style.display = 'none';
+let nums = document.querySelector('.numbers');
+let cardOne = document.querySelector('#card-atall');
+let cardTwo = document.querySelector('#card-atall2');
+let numP = document.querySelector('.num-p')
+
+
+function esconde() {
+    cardOne.style.display = 'none';
+    cardTwo.style.display = 'block';
 }
+
+
+nums.addEventListener('click', function onClick(event){
+    nums.style.backgroundColor = 'hsl(25, 97%, 53%)';
+    numP.style.color = 'white';
+    // nums[i].style.color = 'white';
+})
+
+
+// function mostra() {
+// }
+
+// function mudaCor() {
+//     for(let i = 0;i < nums.length;i++) {
+//         nums[i].setAttribute('style', 'color: red');
+//     }
+// }
