66f5f6a169a675f3c50adc00d62b575c82b93c5a
Vidotti <vidottizzz@gmail.com>
2023-01-21 17:23:52 -0300

    finished the project, changed some original styles

--- script.js
+++ script.js
@@ -3,34 +3,36 @@ let cardOne = document.querySelector('#card-atall');
 let cardTwo = document.querySelector('#card-atall2');
 let numP = document.querySelector('.num-p')
 let btnsbm = document.querySelector('#botaosubmit');
-
+let rated = document.querySelector('#rated');
 
 function esconde() {
     cardOne.style.display = 'none';
     cardTwo.style.display = 'block';
 
 }
-// document.getElementById("prevent").addEventListener("click",(event)=>{
-//     event.preventDefault();
-// })
-
 
 function validateForm() {
     var radios = document.getElementsByName("options");
     var formValid = false;
     
     var i = 0;
+    rated.innerHTML = radios[i].tabIndex;
     while (!formValid && i < radios.length) {
         if (radios[i].checked) formValid = true;
-        i++;        
+        if(radios[i].checked){
+            rate_value = radios[i].value;
+            console.log(rate_value);
+        }
+        i++;       
     }
     
     if (!formValid)
     {
-        alert("Must check some option!");
+        alert("Escolha uma opção");
         return formValid;
     }if(formValid) {
         esconde();
+        rated.innerHTML = rate_value;
     }
     
     
