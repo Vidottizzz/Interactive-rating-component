42699ac0411f7b6c19744882836485ccbd4c1629
Vidotti <vidottizzz@gmail.com>
2023-01-20 23:12:33 -0300

    validation done

--- script.js
+++ script.js
@@ -2,19 +2,39 @@ let nums = document.querySelector('.numbers');
 let cardOne = document.querySelector('#card-atall');
 let cardTwo = document.querySelector('#card-atall2');
 let numP = document.querySelector('.num-p')
+let btnsbm = document.querySelector('#botaosubmit');
 
 
 function esconde() {
     cardOne.style.display = 'none';
     cardTwo.style.display = 'block';
+
 }
+// document.getElementById("prevent").addEventListener("click",(event)=>{
+//     event.preventDefault();
+// })
 
 
-nums.addEventListener('click', function onClick(event){
-    nums.style.backgroundColor = 'hsl(25, 97%, 53%)';
-    numP.style.color = 'white';
-    // nums[i].style.color = 'white';
-})
+function validateForm() {
+    var radios = document.getElementsByName("options");
+    var formValid = false;
+    
+    var i = 0;
+    while (!formValid && i < radios.length) {
+        if (radios[i].checked) formValid = true;
+        i++;        
+    }
+    
+    if (!formValid)
+    {
+        alert("Must check some option!");
+        return formValid;
+    }if(formValid) {
+        esconde();
+    }
+    
+    
+}
 
 
 // function mostra() {
